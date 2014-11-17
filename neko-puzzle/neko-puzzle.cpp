e9a7d0794af281f0491f4501e5d584e6d4681ab2
sugawara5110 <satohi.sugawara19790115@gmail.com>
2014-11-17 22:35:58 +0900

    変更
    変更

--- neko-puzzle/neko-puzzle.cpp
+++ neko-puzzle/neko-puzzle.cpp
@@ -1,3 +1,8 @@
+//*****************************************************************************************//
+//**                                                                                     **//
+//**                   　　　　　　スライドパズル                                        **//
+//**                                                                                     **//
+//*****************************************************************************************//
 #include "DxLib.h"
 #include <string.h>
 #include <stdio.h>
@@ -5,11 +10,11 @@
 #include <time.h>
 
 int nekog[160000],tempx,tempy,g1;  //一時保管
-int i,j,k;                         //for文
+int i,j,k,a;                         //for文
 int frg=0,frga;                   //キー入力,フラグ
 int Color;                       //色
 int size=0;                      //paras添え字
-int spacex,spacey;              //スペース座標
+int space[2];                  //スペース座標0:x 1:y
 
 typedef struct {  //画像座標宣言 
    int nx;        //x座標
@@ -40,6 +45,247 @@ typedef struct {   //ブロック個数毎のパラメーター宣言
 		{160000,400,1,519,449,200000,159999,319999,1} 
 		}; 
 		
+int mov(int *space,int frg,int autof){  //移動処理関数宣言
+          int mv; //auto_matic関数用移動スピード
+	  int mov_p=0; //移動実施判定用
+	  k=-1; //初期化
+	  for(i=0;i<paras[size].soeji;i++){    
+	         //↓auto_matic()関数のブロック衝突判定処理
+		 if(autof==1 && nekog[i]==1){k=-1;continue;}
+	         //↓移動キー毎に全ブロックとスペースの座標比較,移動対象ブロック決定(添え字k)　
+	         if(frg==1 && neko[i].nx==space[0]+paras[size].size_a && neko[i].ny==space[1]    ){k=i;break;} 
+	         if(frg==2 && neko[i].nx==space[0]-paras[size].size_a && neko[i].ny==space[1]    ){k=i;break;}
+	         if(frg==3 && neko[i].nx==space[0]     && neko[i].ny==space[1]+paras[size].size_a){k=i;break;}
+	         if(frg==4 && neko[i].nx==space[0]     && neko[i].ny==space[1]-paras[size].size_a){k=i;break;}
+	  }  //for終わり
+	  
+
+          if(k>=0){   //移動対象ブロック決定時のみ実行
+               if(autof==0)mv=paras[size].move;
+	       if(autof==1){mv=paras[size].move;DrawBox(0,0,640,50,0,TRUE);
+	                    DrawFormatString( 0,16, Color, "[0]ハイスピードON" );}
+	       if(autof==1 && CheckHitKey(KEY_INPUT_NUMPAD0)==1)mv=paras[size].size_a;
+	        
+ 	       while( neko[k].nx!=space[0] || neko[k].ny!=space[1]){   //移動中座標更新処理,スペース座標に到達したら終了)
+	        
+	            
+	            if(frg==1){neko[k].nx-=mv; //座標値更新
+		      if(size!=5){      //size==0～4の処理(size==5は1回で移動完了の為、ラストのみ処理)
+			DrawBox(neko[k].nx+paras[size].size_a,space[1],     //移動元のスペース画像処理
+			        space[0]+paras[size].size_a*2,space[1]+paras[size].size_a,
+				0,TRUE);
+		        DrawGraph( neko[k].nx,neko[k].ny, neko[k].ng, TRUE );//移動ブロック画像処理
+		      } 
+		    }
+		    
+		    if(frg==2){neko[k].nx+=mv;
+		      if(size!=5){
+		        DrawBox(space[0]-paras[size].size_a,space[1],neko[k].nx-1,neko[k].ny+paras[size].size_a,0,TRUE);
+			DrawGraph( neko[k].nx,neko[k].ny, neko[k].ng, TRUE );
+		      } 
+		    }
+		    
+		   if(frg==3){neko[k].ny-=mv;
+		     if(size!=5){
+			DrawBox(space[0],neko[k].ny+paras[size].size_a,
+			        space[0]+paras[size].size_a,space[1]+paras[size].size_a*2,
+				0,TRUE);
+		        DrawGraph( neko[k].nx,neko[k].ny, neko[k].ng, TRUE );
+		     } 
+	           }
+		   
+		   if(frg==4){neko[k].ny+=mv;
+		     if(size!=5){
+		        DrawBox(space[0],space[1]-paras[size].size_a,space[0]+paras[size].size_a,neko[k].ny-1,0,TRUE);
+			DrawGraph( neko[k].nx,neko[k].ny, neko[k].ng, TRUE );
+		     }
+		   } 
+		  ScreenFlip(); //表画面描写
+		   
+		    
+	       }//while終わり
+	        
+		
+		//↓移動完了後スペース座標更新
+		if(frg==1)space[0]+=paras[size].size_a;
+		if(frg==2)space[0]-=paras[size].size_a;
+		if(frg==3)space[1]+=paras[size].size_a;
+		if(frg==4)space[1]-=paras[size].size_a;
+		if(size!=5){    //スペース座標==画像座標の時点でwhile終了の為、==時の画像処理
+		   DrawBox(space[0],space[1],space[0]+paras[size].size_a,space[1]+paras[size].size_a,0,TRUE);
+		   DrawGraph( neko[k].nx,neko[k].ny, neko[k].ng, TRUE );
+		}else{
+		   DrawPixel( space[0],space[1],0 );    //size==5は1回で処理完了の為表示1回のみ
+		   DrawPixel( neko[k].nx,neko[k].ny, neko[k].ng );
+		 }
+          } //if終わり
+	  if(k>=0)mov_p=1;
+	  k=-1; //移動完了後kを初期化
+	  return mov_p;	       
+ }  //mov()終わり
+ 
+
+
+void auto_space_mov(int *space,int *x,int *y){//スペースを目的に隣接するまで自動移動
+     int f;//mov()結果
+     int s=paras[size].size_a;
+     while(1){
+          if((space[0]==*x && space[1]==*y-s)||(space[0]==*x+s && space[1]==*y-s)|| //目的地に隣接
+             (space[0]==*x+s && space[1]==*y)||(space[0]==*x+s && space[1]==*y+s)||
+	     (space[0]==*x && space[1]==*y+s)||(space[0]==*x-s && space[1]==*y+s)||
+	     (space[0]==*x-s && space[1]==*y)||(space[0]==*x-s && space[1]==*y-s))break;
+        
+          if(space[0]-*x>0)f=mov(space,2,1);//スペース右位置関係別に引数を変えmov関数を呼び出す
+	  if(space[0]-*x>0 && f==0){mov(space,3,1);continue;} 
+	  if(space[0]-*x<0)mov(space,1,1);//スペース左
+	  if(space[1]-*y>0)mov(space,4,1);//スペース下 
+          if(space[1]-*y<0)mov(space,3,1);//スペース上
+           
+     }//while終わり
+}//auto_space_mov終わり
+                                
+
+
+ 
+void auto_round(int *x,int *y,int cx,int cy){ //スペース回転,ブロック移動関数
+     int s=paras[size].size_a;
+     int fr=0; //回転方向 0:時計 0以外:反時計 movp==0(移動未実施)の度切り替わる
+     int movp=0;//回転方向切換フラグ
+     auto_space_mov(space,x,y);
+     
+     while( *x!=cx || *y!=cy){
+          if(space[0]==*x && space[1]==*y-s){       //スペース上
+	    if(*y-cy>0){mov(space,3,1);continue;}//目的地上方向
+	    if(movp==0)fr=!fr;  //移動未実施時,回転方向切換
+	    if(fr==0)movp=mov(space,1,1);else movp=mov(space,2,1); //回転方向分岐,右,左
+	  }
+          if(space[0]==*x+s && space[1]==*y-s){     //スペース右上
+            if(movp==0)fr=!fr;  //移動未実施時,回転方向切換
+	    if(fr==0)movp=mov(space,3,1);else movp=mov(space,2,1); //回転方向分岐,下,左
+	  }
+          if(space[0]==*x+s && space[1]==*y){       //スペース右
+	    if(*x-cx<0){mov(space,2,1);continue;}//目的地右方向
+	    if(movp==0)fr=!fr;  //移動未実施時,回転方向切換
+	    if(fr==0)movp=mov(space,3,1);else movp=mov(space,4,1); //回転方向分岐,下,上
+	  }
+          if(space[0]==*x+s && space[1]==*y+s){     //スペース右下
+	    if(movp==0)fr=!fr;  //移動未実施時,回転方向切換
+	    if(fr==0)movp=mov(space,2,1);else movp=mov(space,4,1); //回転方向分岐,左,上
+	  }
+          if(space[0]==*x && space[1]==*y+s){       //スペース下
+	    if(*y-cy<0){mov(space,4,1);continue;}//目的地下方向
+	    if(movp==0)fr=!fr;  //移動未実施時,回転方向切換
+	    if(fr==0)movp=mov(space,2,1);else movp=mov(space,1,1); //回転方向分岐,左,右
+	  }
+          if(space[0]==*x-s && space[1]==*y+s){     //スペース左下
+	    if(movp==0)fr=!fr;  //移動未実施時,回転方向切換
+	    if(fr==0)movp=mov(space,4,1);else movp=mov(space,1,1); //回転方向分岐,上,右
+	  }
+          if(space[0]==*x-s && space[1]==*y){       //スペース左
+	    if(*x-cx>0){mov(space,1,1);continue;}//目的地左方向
+	    if(movp==0)fr=!fr;  //移動未実施時,回転方向切換
+	    if(fr==0)movp=mov(space,4,1);else movp=mov(space,3,1); //回転方向分岐,上,下
+	  }
+          if(space[0]==*x-s && space[1]==*y-s){     //スペース左上
+	    if(movp==0)fr=!fr;  //移動未実施時,回転方向切換
+	    if(fr==0)movp=mov(space,1,1);else movp=mov(space,3,1); //回転方向分岐,右,下
+	  }
+	  
+          
+      } //while終わり
+     
+
+
+}//auto_round()終わり
+
+
+                     
+void auto_matic(int *space,int tateyoko,int range,int j){    //オート関数宣言
+              if(range==tateyoko){ //初期化
+                for(a=0;a<paras[size].soeji;a++){ 
+                    nekog[a]=0; //mov()関数内の移動禁止フラグ
+                } 
+              } 
+       
+              
+        huka1:   
+	      //↓範囲内上1列1回目処理 
+              for(a=j+1;a<range+j;a++){  
+	          auto_round(&neko[a].nx,&neko[a].ny,nekocopy[a-1].nx,nekocopy[a-1].ny); 
+	          nekog[a]=1;	//移動禁止ON
+	      } //for終わり
+	 	      
+	      //↓移動不可状態処理
+	      if((neko[j].nx==nekocopy[range+j-1].nx && neko[j].ny==nekocopy[range+j-1].ny)||
+	         (neko[j].nx==nekocopy[range+j-1+tateyoko].nx && neko[j].ny==nekocopy[range+j-1+tateyoko].ny &&
+	          space[0]==nekocopy[range+j-1].nx && space[1]==nekocopy[range+j-1].ny )){
+		  for(a=j+1;a<range+j;a++){
+		    nekog[a]=0;
+		  }
+		  auto_round(&neko[j].nx,&neko[j].ny,nekocopy[range+j-1+tateyoko*2].nx,nekocopy[range+j-1+tateyoko*2].ny);
+	          goto huka1;}	  
+	      
+	      //↓範囲内上1列左1個処理
+	      auto_round(&neko[j].nx,&neko[j].ny,nekocopy[j+tateyoko].nx,nekocopy[j+tateyoko].ny);
+	      nekog[j]=1;  //移動禁止ON
+	      
+	      //↓範囲内上1列2回目処理
+	      for(a=range+j-1;a>=j+1;a--){
+	          nekog[a]=0;        //移動前移動禁止OFF
+		  auto_round(&neko[a].nx,&neko[a].ny,nekocopy[a].nx,nekocopy[a].ny);
+		  nekog[a]=1;        //移動後移動禁止ON
+	      }//for終わり
+	      
+	      if(range==2){nekog[j]=0;mov(space,3,1);mov(space,1,1);mov(space,1,1);return;}      //完了
+	      
+	      //↓範囲内上1列左1個処理
+	      nekog[j]=0;            //移動前移動禁止OFF
+	      auto_round(&neko[j].nx,&neko[j].ny,nekocopy[j].nx,nekocopy[j].ny);
+              nekog[j]=1;            //移動後移動禁止ON
+	      
+	      
+	      j+=tateyoko;         //for文初期値変更
+	      range--;             //範囲変更
+              
+	      
+	huka2:     
+	      //↓範囲内左1列1回目処理
+	      for(a=j+tateyoko;a<=j+tateyoko*(range-1);a+=tateyoko){
+	          auto_round(&neko[a].nx,&neko[a].ny,nekocopy[a-tateyoko].nx,nekocopy[a-tateyoko].ny);
+	          nekog[a]=1;  //移動禁止ON
+	      } //for終わり
+              
+	      //↓移動不可状態処理
+	      if((neko[j].nx==nekocopy[j+tateyoko*(range-1)].nx && neko[j].ny==nekocopy[j+tateyoko*(range-1)].ny)||
+	         (neko[j].nx==nekocopy[j+tateyoko*(range-1)+1].nx && neko[j].ny==nekocopy[j+tateyoko*(range-1)+1].ny &&
+		  space[0]==nekocopy[j+tateyoko*(range-1)].nx && space[1]==nekocopy[j+tateyoko*(range-1)].ny)){
+		  for(a=j+tateyoko;a<=j+tateyoko*(range-1);a+=tateyoko){
+		  nekog[a]=0;
+		  }
+		  auto_round(&neko[j].nx,&neko[j].ny,nekocopy[j+tateyoko*(range-1)+2].nx,nekocopy[j+tateyoko*(range-1)+2].ny);
+		  goto huka2;} 
+	      
+	      //↓範囲内左1列上1個処理
+	      auto_round(&neko[j].nx,&neko[j].ny,nekocopy[j+1].nx,nekocopy[j+1].ny);
+	      nekog[j]=1;         //移動禁止ON
+	      //↓範囲内左1列2回目処理
+	      for(a=j+tateyoko*(range-1);a>=j+tateyoko;a-=tateyoko){
+	          nekog[a]=0;   //移動禁止OFF
+		  auto_round(&neko[a].nx,&neko[a].ny,nekocopy[a].nx,nekocopy[a].ny);
+		  nekog[a]=1;   //移動禁止ON
+	      }//for終わり
+	      //↓範囲内左1列上1個処理
+	      nekog[j]=0;  //移動禁止OFF
+	      auto_round(&neko[j].nx,&neko[j].ny,nekocopy[j].nx,nekocopy[j].ny);
+              nekog[j]=1;  //移動禁止ON
+	      
+	      j++; //for文初期値変更
+         //↓再帰呼び出し  
+         auto_matic(space,tateyoko,range,j);
+
+}//auto_matic()終わり
+     
+    
 
 
 void narabikae(){              //並び替え関数宣言
@@ -88,69 +334,25 @@ void narabikae(){              //並び替え関数宣言
 }  //narabikae()終わり	
 
 
-void shuffle(){   //シャッフル関数宣言
-     
-     if(size<=2){       //size==0～2までの処理
-        for(i=0;i<paras[size].soeji;i++){nxy[i]=nekocopy[i];} //完成画像データからコピー
-	neko[paras[size].soeji-1].nx=520;
-        spacex=paras[size].lastposx; //スペース座標初期化
-	spacey=paras[size].lastposy;  
-       for(j=0;j<paras[size].rand_i;j++){  
-          k=rand()%4;  //ランダムで4方向決定
-	  
-	 for(i=0;i<paras[size].rand_s;i++){        
-	   if(k==0 && nxy[i].nx==spacex-paras[size].size_a && nxy[i].ny==spacey) //スペース座標隣接ブロック探索
-	   {
-	      tempx=nxy[i].nx;nxy[i].nx=spacex;spacex=tempx;                     //スペース座標,隣接ブロック座標入れ替え
-	      tempy=nxy[i].ny;nxy[i].ny=spacey;spacey=tempy;
-	   }
-	   if(k==1 && nxy[i].nx==spacex+paras[size].size_a && nxy[i].ny==spacey)  
-	   {
-	      tempx=nxy[i].nx;nxy[i].nx=spacex;spacex=tempx;
-	      tempy=nxy[i].ny;nxy[i].ny=spacey;spacey=tempy;
-	   }
-	   if(k==2 && nxy[i].ny==spacey-paras[size].size_a && nxy[i].nx==spacex)
-	   {
-	      tempx=nxy[i].nx;nxy[i].nx=spacex;spacex=tempx;
-	      tempy=nxy[i].ny;nxy[i].ny=spacey;spacey=tempy;
-	   }
-	   if(k==3 && nxy[i].ny==spacey+paras[size].size_a && nxy[i].nx==spacex)
-	   {
-	      tempx=nxy[i].nx;nxy[i].nx=spacex;spacex=tempx;
-	      tempy=nxy[i].ny;nxy[i].ny=spacey;spacey=tempy;
-	   } 
-	 }//for終わり 
-       } //for終わり
-     
-       do{    //for文終了時のspacex,spaceyの座標をlastposx,lastposyまでブロック交換しながら移動させる
-          for(i=0;i<paras[size].soeji-1;i++){ 
-	   if(nxy[i].nx==spacex+paras[size].size_a && nxy[i].ny==spacey ){tempx=nxy[i].nx;nxy[i].nx=spacex;spacex=tempx;}   
-	  }      
-       }while(spacex<paras[size].lastposx);   
-       
-       do{
-         for(i=0;i<paras[size].soeji-1;i++){ 
-           if(nxy[i].ny==spacey+paras[size].size_a && nxy[i].nx==spacex){tempy=nxy[i].ny;nxy[i].ny=spacey;spacey=tempy;}
-	 }     
-       }while(spacey<paras[size].lastposy);
-      
-      }else{  
-        for(i=0;i<=paras[size].rand_i;i++){         // nxy[]内でnx,nyをランダムでデータ入れ替え,size==3～5の処理
+void shuffle(){   //シャッフル関数宣言  
+        for(i=0;i<paras[size].rand_i;i++){         // nxy[]内でnx,nyをランダムでデータ入れ替え
 	                           //size==5の場合の処理(rand関数max値が足りない為)
 	    if(size==5){j=(rand()%10000)*15+rand()%10014;k=(rand()%10000)*15+rand()%10014;
 	    }else{                 //size==0～4の場合の処理
 	      j=rand()%paras[size].rand_s;   //交換元添え字j
 	      k=rand()%paras[size].rand_s;   //交換先添え字k
+	      if(j==k){i--;continue;}
 	     }  
-	     tempx=nxy[j].nx;              //交換
+	     tempx=nxy[j].nx;    //交換
 	     tempy=nxy[j].ny;
 	     nxy[j].nx=nxy[k].nx;
 	     nxy[j].ny=nxy[k].ny;
 	     nxy[k].nx=tempx;
 	     nxy[k].ny=tempy;
 	} //for終わり  		
-     }
-}   //シャッフル関数終わり 
+     
+}   //shuffle()終わり 
+
 
 int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int){    //メイン関数
         ChangeWindowMode(TRUE), DxLib_Init(), SetDrawScreen( DX_SCREEN_BACK ); //ウィンドウモード変更と初期化と裏画面設定
@@ -227,8 +429,8 @@ start:         //gotoラベル
 	shuffle();    //シャッフル関数
         narabikae();  //並び替え関数
 
-	spacex=paras[size].lastposx;       //スペースx座標初期化
-	spacey=paras[size].lastposy;       //スペースy座標初期化
+	space[0]=paras[size].lastposx;       //スペースx座標初期化
+	space[1]=paras[size].lastposy;       //スペースy座標初期化
 	frg=0;           //移動キー情報
 	k=-1;            //移動対象ブロック添え字
 	frga=0;          //アクティブモード切替キー情報 
@@ -251,14 +453,14 @@ start:         //gotoラベル
 	       if( CheckHitKey(KEY_INPUT_NUMPAD6)==1){InitGraph();goto start;} //読み込み済みグラフィックデータ削除,goto
                
 	       if(frga==0){           //モード表示切替
-	        DrawBox(0,0,200,14,0,TRUE);
-	        DrawFormatString( 0, 0, Color, "アクティブモードOFF") ;
-	          }else {DrawBox(0,0,200,14,0,TRUE);
-		         DrawFormatString( 0, 0, Color, "アクティブモードON") ;
+	        DrawBox(0,0,500,16,0,TRUE);
+	        DrawFormatString( 0, 0, Color, "アクティブモードOFF [右エンター]切り替え") ;
+	          }else {DrawBox(0,0,500,16,0,TRUE);
+		         DrawFormatString( 0, 0, Color, "アクティブモードON [右エンター]切り替え") ;
 	                }
-	        
-	       DrawFormatString( 0, 30, Color, "[1]キー元通り　[2]キー再並び替え　[6]ブロック個数変更　矢印キーブロック移動") ;
-               DrawFormatString( 0, 15, Color, "右エンターキー切り替え") ;
+	       DrawBox(0,16,500,50,0,TRUE); 
+	       DrawFormatString( 0, 32, Color, "[3]オートモードスタート ") ;
+               DrawFormatString( 0, 17, Color, "[1]元通り　[2]シャッフル  [6]ブロック個数変更　[↑↓→←]ブロック移動") ;
 	       ScreenFlip();//表画面描写
 	       
 	       if( CheckHitKey( KEY_INPUT_LEFT )==1)frg=1;      //左  ブロック移動キー検出
@@ -266,15 +468,19 @@ start:         //gotoラベル
 	       if( CheckHitKey( KEY_INPUT_UP )==1)frg=3;         //上
 	       if( CheckHitKey( KEY_INPUT_DOWN )==1)frg=4;      //下
 	      
-	      
+	       if( CheckHitKey( KEY_INPUT_NUMPAD3)==1)auto_matic(space,paras[size].tateyoko,paras[size].tateyoko,0);
+	          
+                   
+		 
+		  
 	      
 	      
 	       if( CheckHitKey( KEY_INPUT_NUMPAD1)==1){               //全ブロック位置元通り処理↓
 	           
 	           for(i=0;i<paras[size].soeji;i++){nxy[i]=nekocopy[i];} //完成画像データからコピー
 	           narabikae();                   //並び替え関数
-		   spacex=paras[size].lastposx;    //データ初期化
-		   spacey=paras[size].lastposy;
+		   space[0]=paras[size].lastposx;    //データ初期化
+		   space[1]=paras[size].lastposy;
 		   frg=0;
 		   k=-1; 
 		   frga=0;
@@ -284,90 +490,33 @@ start:         //gotoラベル
 	         
 	           shuffle();       //シャッフル関数
 		   narabikae();       //並び替え関数
-		   spacex=paras[size].lastposx;   //データ初期化
-		   spacey=paras[size].lastposy;
+		   space[0]=paras[size].lastposx;   //データ初期化
+		   space[1]=paras[size].lastposy;
 		   frg=0;
 		   k=-1; 
 		   frga=0;  
 	       }   //if終わり
-	     }while( CheckHitKeyAll() == 0 && frga==0 ); //do-while終わり アクティブモードON(frga==1)の場合連続で移動処理実行
+	     }while( CheckHitKeyAll() == 0 && frga==0 ); //do-while終わり アクティブモードON(frga!=0)の場合連続で移動処理実行
 	    }  //if終わり  
-             for(i=0;i<paras[size].soeji;i++){    
-	       
-	       //↓移動キー毎に全ブロックとスペースの座標比較,移動対象ブロック決定(添え字k)　
-	       if(frg==1 && neko[i].nx==spacex+paras[size].size_a && neko[i].ny==spacey    ){k=i;} 
-	       if(frg==2 && neko[i].nx==spacex-paras[size].size_a && neko[i].ny==spacey    ){k=i;}
-	       if(frg==3 && neko[i].nx==spacex     && neko[i].ny==spacey+paras[size].size_a){k=i;}
-	       if(frg==4 && neko[i].nx==spacex     && neko[i].ny==spacey-paras[size].size_a){k=i;}
-	     }  //for終わり
+             
 	     
-	      //↓移動処理
-	     if(k>=0){   //移動対象ブロック決定時のみ実行
 	      
-	       while( neko[k].nx!=spacex || neko[k].ny!=spacey){   //移動中座標更新処理,スペース座標に到達したら終了)
-	        
-	            
-	            if(frg==1){neko[k].nx-=paras[size].move; //座標値更新
-		      if(size!=5){      //size==0～4の処理(size==5は1回で移動完了の為、ラストのみ処理)
-			DrawBox(neko[k].nx+paras[size].size_a,spacey,     //移動元のスペース画像処理
-			        spacex+paras[size].size_a*2,spacey+paras[size].size_a,
-				0,TRUE);
-		        DrawGraph( neko[k].nx,neko[k].ny, neko[k].ng, TRUE );//移動ブロック画像処理
-		      } 
-		    }
-		    
-		    if(frg==2){neko[k].nx+=paras[size].move;
-		      if(size!=5){
-		        DrawBox(spacex-paras[size].size_a,spacey,neko[k].nx-1,neko[k].ny+paras[size].size_a,0,TRUE);
-			DrawGraph( neko[k].nx,neko[k].ny, neko[k].ng, TRUE );
-		      } 
-		    }
-		    
-		   if(frg==3){neko[k].ny-=paras[size].move;
-		     if(size!=5){
-			DrawBox(spacex,neko[k].ny+paras[size].size_a,
-			        spacex+paras[size].size_a,spacey+paras[size].size_a*2,
-				0,TRUE);
-		        DrawGraph( neko[k].nx,neko[k].ny, neko[k].ng, TRUE );
-		     } 
-	           }
-		   
-		   if(frg==4){neko[k].ny+=paras[size].move;
-		     if(size!=5){
-		        DrawBox(spacex,spacey-paras[size].size_a,spacex+paras[size].size_a,neko[k].ny-1,0,TRUE);
-			DrawGraph( neko[k].nx,neko[k].ny, neko[k].ng, TRUE );
-		     }
-		   } 
-		  ScreenFlip(); //表画面描写
-		   
-		    
-	       }//while終わり
-	        
-		
-		//↓移動完了後スペース座標更新
-		if(frg==1)spacex+=paras[size].size_a;
-		if(frg==2)spacex-=paras[size].size_a;
-		if(frg==3)spacey+=paras[size].size_a;
-		if(frg==4)spacey-=paras[size].size_a;
-		if(size!=5){    //スペース座標==画像座標の時点でwhile終了の為、==時の画像処理
-		   DrawBox(spacex,spacey,spacex+paras[size].size_a,spacey+paras[size].size_a,0,TRUE);
-		   DrawGraph( neko[k].nx,neko[k].ny, neko[k].ng, TRUE );
-		}else{
-		   DrawPixel( spacex,spacey,0 );    //size==5は1回で処理完了の為表示1回のみ
-		   DrawPixel( neko[k].nx,neko[k].ny, neko[k].ng );
-		 }
-		if(frga==0)frg=0; //移動完了後アクティブモードoff時frg=0に更新させ連続移動させない
-	        k=-1; //移動完了後kを初期化
-	     } //if終わり 
-	      
-	       
-	     
 	    
-	 
-	 
+	     
+	     mov(space,frg,0); //移動処理
+	     
+	     
+	        
+            if(frga==0)frg=0; //移動完了後アクティブモードoff時frg=0に更新させ連続移動させない
+	    
 	 
 	 }  //while終わり
         
         DxLib_End(); // DXライブラリ終了処理
         return 0;
 }     // winmain終わり
+
+//********************************************************************************************************//
+//**** libjpeg　Copyright (C) 1991-2013, Thomas G. Lane, Guido Vollbeding.  ******************************//
+//**** DX Library Copyright (C) 2001-2014 Takumi Yamada.   ***********************************************//
+//********************************************************************************************************//
